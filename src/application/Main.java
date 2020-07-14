5dc2723f880a310843772b83cd95e4ec9401b4b6
AndreSylva <andre.lucyo2@gmail.com>
2020-07-14 00:47:08 -0300

    Criado About view - e Função de carregamento dentro da mainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,9 +8,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-//build config :   --module-path H:\Noot_DELL\Dev_Software\WorkSpaceJAVA\Libs-eclipse\javafx-sdk\lib --add-modules=javafx.fxml,javafx.controls
+// build config : --module-path H:\Noot_DELL\Dev_Software\WorkSpaceJAVA\Libs-eclipse\javafx-sdk\lib
+// --add-modules=javafx.fxml,javafx.controls
+
+//Ref. https://github.com/acenelio/workshop-javafx-jdbc
 
 public class Main extends Application {
+
+	//Expoe a Scene para abrir novas telas
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage)
@@ -19,18 +25,18 @@ public class Main extends Application {
 		{
 			// 1- Lê/Carrega o FXML - Passar o caminho completo do FXML da VIEW/Tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			
-			// 2 - Carregar o control principal com a VIEW:		
+
+			// 2 - Carregar o control principal com a VIEW:
 			ScrollPane scrollPane = loader.load();
-			//Faz com que o ScrollPane se ajuste a tela:
+			// Faz com que o ScrollPane se ajuste a tela:
 			scrollPane.setFitToHeight(true);
-			scrollPane.setFitToWidth(true);			
-			
+			scrollPane.setFitToWidth(true);
+
 			// 3 - Coloca o FXML em uma Scene
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 
 			// CSS de Styles:
-			//mainScene.getStylesheets().add("/application/application.css");
+			// mainScene.getStylesheets().add("/application/application.css");
 
 			// 4 - Coloca a Scene em uma janela
 			primaryStage.setScene(mainScene);
@@ -38,13 +44,18 @@ public class Main extends Application {
 
 			// 5 - Mostra a tela
 			primaryStage.show();
-					
-			
+
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	//Retorna a Scene principal:
+	public static Scene getMainScene()
+	{
+		return mainScene;
 	}
 
 	public static void main(String[] args)
--- src/gui/MainViewCoontroller.java
+++ src/gui/MainViewCoontroller.java
@@ -1,51 +1,93 @@
 /**
  * @author Andre
- * @date 13/07/2020 10:10:11
  * 
+ * @date 13/07/2020 10:10:11
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewCoontroller implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction()
 	{
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction()
 	{
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAbutAction()
 	{
-		System.out.println("onMenuItemAbutAction");
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1)
 	{
-		
-		
+
+	}
+
+	private synchronized void loadView(String absoluteName)
+	{
+		try
+		{
+			//Criar um loader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			//Recebe um VBox do parametro
+			VBox newVBox = loader.load();
+
+			//Recebe uma Scena do Main
+			Scene mainScene = Main.getMainScene();
+			
+			//Navega e guarda a VBox do Main:
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			//Guarda o primeiro filho da VBox do Main: o menu itens
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			//Limpa os filhos da VBoxMain			
+			mainVBox.getChildren().clear();
+			
+			//Adiciona novamente o menuItens:
+			mainVBox.getChildren().add(mainMenu);
+			
+			//Adiciona todos os filhos da nova tela:
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch (IOException e)
+		{
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
