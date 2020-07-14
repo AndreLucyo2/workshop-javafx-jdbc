45cf62cce10cafa3568a877c21d9d21efc882e14
AndreSylva <andre.lucyo2@gmail.com>
2020-07-13 22:09:17 -0300

    Main view design - com macete de auto ajuste a tela

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 //build config :   --module-path H:\Noot_DELL\Dev_Software\WorkSpaceJAVA\Libs-eclipse\javafx-sdk\lib --add-modules=javafx.fxml,javafx.controls
@@ -19,11 +19,15 @@ public class Main extends Application {
 		{
 			// 1- Lê/Carrega o FXML - Passar o caminho completo do FXML da VIEW/Tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			// 2 - Carregar o control Parent: Controle principal, pai de todos neste exemplo um AnchorPane		
-			Parent parent = loader.load();
+			
+			// 2 - Carregar o control principal com a VIEW:		
+			ScrollPane scrollPane = loader.load();
+			//Faz com que o ScrollPane se ajuste a tela:
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);			
 			
 			// 3 - Coloca o FXML em uma Scene
-			Scene mainScene = new Scene(parent);
+			Scene mainScene = new Scene(scrollPane);
 
 			// CSS de Styles:
 			//mainScene.getStylesheets().add("/application/application.css");
