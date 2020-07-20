774f3608576d8d4a0c6fa8b099d16fb365c9b2c5
AndreSylva <andre.lucyo2@gmail.com>
2020-07-20 10:31:44 -0300

    SellerForm - Clonado form Seller a partir do Department

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,222 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+// or form tem dupla função, adicionar e atualizar
+public class SellerFormController implements Initializable {
+
+	private Seller entity;
+
+	// Injeta a dependencia do serviço do SelleroService
+	private SellerService service;
+
+	// é a classe que emite o evento.
+	// Instancia uma lista vazia que vai receber os obj ouvintes
+	// Guarda uma lista de objetos interessados em receber o evento (Ouvinte)
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label labelErrorName;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	// Injeção de depencia: seta a entidade do departamento
+	public void setSeller(Seller entity)
+	{
+		// O controller vai ter uma instancia do entities departament
+		this.entity = entity;
+	}
+
+	// Injeção de dependencia do serviço
+	public void setSellerService(SellerService service)
+	{
+		this.service = service;
+	}
+
+	// permite outros betos se inscreverem na lista pada poder ouvir/ receber os eventos
+	public void subscribeDataChangeListener(DataChangeListener listener)
+	{
+		// inscreve os interessados em ouvir os eventos quando algo mudar
+		dataChangeListeners.add(listener);
+	}
+
+	@FXML
+	public void onBtSaveAction(ActionEvent event)
+	{
+
+		// Como foi feito a injeção manualmente, validar se nao esta null
+		if (entity == null)
+		{
+			throw new IllegalStateException("Entity was null");
+		}
+
+		// Como foi feito a injeção manualmente, validar se nao esta null
+		if (service == null)
+		{
+			throw new IllegalStateException("Service was null");
+		}
+
+		// Como lida com banco, pode gerar db exception, usar try
+		try
+		{
+			// Pega os dados da tela: tanto para salvar quanto para alterar
+			entity = getFormData();
+
+			// Valida se é insert ou update, e grava / cria no banco
+			service.saveOrUpdate(entity);
+
+			// ==========================================================================
+			// Evento que notifica os ouvintes em caso de salvamento com sucesso
+			notifyDataChangeListeners();
+
+			// Manda fechar a janela atual, pega a referencia do evento
+			Utils.currentStage(event).close();
+
+		}
+		catch (ValidationException e)//implementa a listagem dos erros
+		{
+			//caso acontece um erro de validação:
+			setErrorMessages(e.getErrors());
+		}
+		catch (DbException e)
+		{
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+
+	}
+
+	// Implementação da classe que emite os eventos
+	// Emite o evento para todos os ouvintes ver Seção 23 aula 282
+	private void notifyDataChangeListeners()
+	{
+		// para cada DataChangeListener da minha lista dataChangeListeners faça ... onDataChanged
+		for (DataChangeListener listener : dataChangeListeners)
+		{
+			// Emite um evendo a cada ciclo:
+			listener.onDataChanged();
+		}
+	}
+
+	// Passar os dados da tela para uma instnacia do objeto obj
+	// implamenta as validações e lanca exceptions caso der erro
+	private Seller getFormData()
+	{
+		// Instancia um objeto:
+		Seller obj = new Seller();
+
+		// instancia uma exception de validação, porem ainda nao foi lançada
+		ValidationException exception = new ValidationException("Validation error");
+
+		// Pega os dados da tela
+		// Pega string ja convertendo para int, caos null retorna null
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		// Validação: o campo nome nao pode ser vazio:
+		if (txtName.getText() == null || txtName.getText().trim().equals(""))
+		{
+			// Adiciona uma mensagem de erro na exceção
+			exception.addError("name", "o Campo nao pode ser vazio!!");
+		}
+
+		// pegar o nome da tela:
+		obj.setName(txtName.getText());
+
+		// Testa se a exceção pegando os erros for maior que zero
+		// tese se na coleção tem pelo menos um erro
+		// lana a exceção caso tenha algum erro
+		if (exception.getErrors().size() > 0)
+		{
+			throw exception;
+		}
+
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event)
+	{
+		// fecha a tela, pegando o stage do botão/Event, clicado
+		Utils.currentStage(event).close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb)
+	{
+		// carregar as restrições:
+		initializeNodes();
+	}
+
+	// definira as restrições dos campos:
+	private void initializeNodes()
+	{
+		// Campo id so aceita inteiros
+		Constraints.setTextFieldInteger(txtId);
+		// Campo nome tem comprimento definido
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+	// pega a entidade e popula as caixinhas da tela
+	public void updateFormData()
+	{
+		// testa se nao esta null: Prgramação de fenciva
+		if (entity == null)
+		{
+			// lança uma exceção pois nao foi carregado um department
+			throw new IllegalStateException("Entity was null");
+		}
+
+		// Popular os campos:
+		// converter para string
+		txtId.setText(String.valueOf(entity.getId()));
+
+		// carrega o dado name:
+		txtName.setText(entity.getName());
+	}
+
+	// metodo responsável por pegar as mensagens de erro e mostrar na tela
+	// Recebe uma coleção de erros.
+	private void setErrorMessages(Map<String, String> errors)
+	{
+		// cria um conjunto de campos
+		Set<String> fields = errors.keySet();
+
+		// percorre a lista de campos, percorre a lista, buscanso pelo nome do campo
+		// busca na coleção se possui alguma mensagem com o nome do campo
+		if (fields.contains("name"))
+		{
+			// Mostra o erro em uma label na tela , para exemplo
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -5,6 +5,7 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -21,7 +22,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -29,6 +32,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -153,55 +158,55 @@ public class SellerListController implements Initializable, DataChangeListener {
 	// injeta um objeto departament na view SellerForm
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage)
 	{
-//		try
-//		{
-//			// Logica para abrir um janela de dialogo:
-//			// Criar um loader
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			// Recebe um Panel do parametro: Carregou a view
-//			Pane pane = loader.load();
-//
-//			// =============================================================================
-//			// injeta o departa no controller na view do formulario
-//			// Pega o controller da tela recebida pelo parametro
-//			SellerFormController controller = loader.getController();
-//
-//			// Injeta o objeto no controller
-//			controller.setSeller(obj);
-//
-//			// Injeção de dependencia do service:
-//			controller.setSellerService(new SellerService());
-//
-//			// Padraão de programação observer: é avançado, alto desacoplamento, o objeto que emite o evento nao
-//			// conhece que esta escutando
-//			// Se inscever para receber o evento observado
-//			// Quando o evento for disparado pela classe sera executado o onDataChanged
-//			controller.subscribeDataChangeListener(this);
-//
-//			// Chama o metodo que carrega os dados na tela:
-//			controller.updateFormData();
-//
-//			// =============================================================================
-//			// CRiar um novo stage para a janela modal, um pauco na frente do outro:
-//			Stage dialogStage = new Stage();
-//			// Titulo da tela:
-//			dialogStage.setTitle("Enter Seller data");
-//			// Definir a cena:
-//			dialogStage.setScene(new Scene(pane));
-//			// nao deixa redimencionar:
-//			dialogStage.setResizable(false);
-//			// quem é o stage pai desta janela: pega do parametro
-//			dialogStage.initOwner(parentStage);
-//			// definir se sera modal: bloqueia a janela anterior:
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			// Mostra a janela:
-//			dialogStage.showAndWait();
-//		}
-//		catch (IOException e)
-//		{
-//
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try
+		{
+			// Logica para abrir um janela de dialogo:
+			// Criar um loader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			// Recebe um Panel do parametro: Carregou a view
+			Pane pane = loader.load();
+
+			// =============================================================================
+			// injeta o departa no controller na view do formulario
+			// Pega o controller da tela recebida pelo parametro
+			SellerFormController controller = loader.getController();
+
+			// Injeta o objeto no controller
+			controller.setSeller(obj);
+
+			// Injeção de dependencia do service:
+			controller.setSellerService(new SellerService());
+
+			// Padraão de programação observer: é avançado, alto desacoplamento, o objeto que emite o evento nao
+			// conhece que esta escutando
+			// Se inscever para receber o evento observado
+			// Quando o evento for disparado pela classe sera executado o onDataChanged
+			controller.subscribeDataChangeListener(this);
+
+			// Chama o metodo que carrega os dados na tela:
+			controller.updateFormData();
+
+			// =============================================================================
+			// CRiar um novo stage para a janela modal, um pauco na frente do outro:
+			Stage dialogStage = new Stage();
+			// Titulo da tela:
+			dialogStage.setTitle("Enter Seller data");
+			// Definir a cena:
+			dialogStage.setScene(new Scene(pane));
+			// nao deixa redimencionar:
+			dialogStage.setResizable(false);
+			// quem é o stage pai desta janela: pega do parametro
+			dialogStage.initOwner(parentStage);
+			// definir se sera modal: bloqueia a janela anterior:
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			// Mostra a janela:
+			dialogStage.showAndWait();
+		}
+		catch (IOException e)
+		{
+
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
